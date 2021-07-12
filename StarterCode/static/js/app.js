99019603b3d3caa97f7b2d560636b13e95ba3c53
Cleveland <eclevela@richmond.edu>
2021-07-12 15:39:23 -0400

    adding working code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -16,6 +16,25 @@
     var result = resultsArray[0];
     console.log(result);
 
+    var washFreq = result.wfreq;
+    console.log(washFreq);
+
+    // build indicator/gauge
+
+    var gaugeData = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: washFreq,
+        title: { text: "Belly Button Wash Frequency <br> Scrubs Per Week" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: { axis: { range: [null, 9] } }
+      }
+    ];
+    
+    var gaugeLayout = { width: 600, height: 400 };
+    Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
     var panel = d3.select("#sample-metadata");
 
     // ensure the panel is clear
@@ -32,7 +51,7 @@
 
     function buildCharts (sample) {
       d3.json("samples.json").then(data => {
-        // console.log(data);
+        console.log(data);
         // parse data out to variables
         // * Use `sample_values` as the values for the bar chart.
         // * Use `otu_ids` as the labels for the bar chart.
@@ -41,12 +60,83 @@
         var samples = data.samples;
         console.log(samples);
 
-        var resultsArray = samples.filter(sampleObj.id == sample);
+        var resultsArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = resultsArray[0];
 
-      console.log(resultsArray);
-        // var sample_values = 
-        // var otu_ids =
-        // var otu_labels =
+        console.log(result);
+        var sample_values = result.sample_values;
+          console.log(sample_values)
+        var otu_ids = result.otu_ids;
+          console.log(otu_ids)
+        var otu_labels = result.otu_labels;
+          console.log(otu_labels)
+
+     
+      // Build bar chart
+        // Use slice to get the top ten data
+
+        var yticks = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+
+        var barData = [{
+          y: yticks,
+          x: sample_values.slice(0,10).reverse(),
+          text: otu_labels.slice(0,10).reverse(),
+          type: "bar",
+          orientation: "h",
+
+        }];
+
+        barLayout = {
+          title: "Top Ten Bacteria Cultures Found",
+          margin: {
+            t: 30,
+            r: 0,
+            b: 30,
+            l: 75
+          },
+
+        };
+
+
+
+
+        Plotly.newPlot("bar", barData, barLayout);
+
+      // Build bubble chart
+
+        var bubbleData = [{
+          x: otu_ids,
+          y: sample_values,
+          
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size:sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+          },
+          
+          orientation: "h",
+        }];
+
+        var bubbleLayout = {
+            title:"Bacteria Cultures Per Sample",
+            margin: {
+              t: 30,
+              r: 10,
+              b: 30,
+              l: 75,
+            },
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+        };
+
+        
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+
+
+
 
       });
 
@@ -56,10 +146,30 @@
     };
 
 function init() {
+  var pullDownMenu = d3.select("#selDataset")
+  d3.json("samples.json").then(data => {
+    var names = data.names;
+      console.log(data.names);
+
+      names.forEach((name) => {
+        pullDownMenu
+          .append("option")
+          .property("value", name)
+          .text(name);
+
+      })
+    
+
+  });
+
   buildMetadata(940);
   buildCharts(940);
 }
 
+function optionChanged(nextSample) {
+  buildMetadata(nextSample);
+  buildCharts(nextSample);
+}
 init();
 
 
